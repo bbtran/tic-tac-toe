9de1d8293462dc5bad4bd4310468cf30c4d11c8f
Benjamin Tran <benjamin.tran25@gmail.com>
2017-02-22 11:08:57 -0800

    [ FIX ] Handle edge cases. Add in reset handling

--- client/tictactoe.js
+++ client/tictactoe.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  function Player(xOrO) {
+    this.symbol = xOrO;
+    this.playerPicks = [];
+  }
+  Player.prototype.constructor = Player;
+  
+  var pickCount = 0;
+  var winner = null;
+  var current;
+  var player1 = new Player("X");
+  var player2 = new Player("O");
+
+  //WINNING OUTCOMES
   var winning = [
     ['r1c1', 'r1c2', 'r1c3'],
     ['r2c1', 'r2c2', 'r2c3'],
@@ -9,20 +22,8 @@ $(document).ready(function() {
     ['r1c1', 'r2c2', 'r3c3'],
     ['r1c3', 'r2c2', 'r3c1'],
   ];
-  var pickCount = 0;
 
-  var winner;
-
-  function Player(xOrO) {
-    this.symbol = xOrO;
-    this.playerPicks = [];
-  }
-  Player.prototype.constructor = Player;
-
-  var current;
-  var player1 = new Player("X");
-  var player2 = new Player("O");
-
+  // RANDOMIZE STARTING PLAYER
   function randomPlayerStart(first, second){
     var startingPlayer;
     var start = Math.ceil(Math.random() * 2);
@@ -34,19 +35,20 @@ $(document).ready(function() {
     return startingPlayer;
   }
 
+  //START GAME BUTTON
   function startGame() {
-    $('.colbox').each(function(item) {
-      $(item).text('');
-    });
+    // $('.colbox').each(function(item) {
+    //   $(item).text('');
+    // });
     current = randomPlayerStart(player1, player2);
     $('.currentPlayer').text(current.symbol + ' Begins First');
   }
-
   $('.startGame').on('click', function(){
     startGame();
     $('.startGame').hide();
   });
 
+  //BOX CLICK HANDLER
   $('.colbox').on('click', function(e){
     var selectedId = this.id || e.target.id;
     if(!current) {
@@ -55,23 +57,42 @@ $(document).ready(function() {
       if($(this).text() === 'X' || $(this).text() === 'O'){
         alert('This box has already been selected');
       } else {
-        current.playerPicks.push(selectedId);
-        $(this).text(''+current.symbol);
-        if(checkWinner(current.playerPicks)){
-          winner = current;
-          $('.container').append('<p> Player '+ winner.symbol+' wins!');
-        }
-        if(current === player1) {
-          current = player2;
+        if(winner !== null) {
+          console.log('inhere');
+          alert('The game has already ended. Please play again');
         } else {
-          current = player1;
+          current.playerPicks.push(selectedId);
+          console.log(this);
+          $(this).text(''+current.symbol);
+          if(checkWinner(current.playerPicks)){
+            winner = current;
+            $('.container').append('<p class="selectedBox"> Player '+ winner.symbol+' wins!');
+          }
+          if(current === player1) {
+            current = player2;
+          } else {
+            current = player1;
+          }
         }
       }
     }
   });
 
+  // PLAY AGAIN HANDLER
+  $('.playAgain').on('click', function(e){
+    e.preventDefault();
+    $('.container .colbox').html('');
+    $('.selectedBox').remove();
+    $('.currentPlayer').text('');
+    $('.startGame').show();
+    pickCount = 0;
+    winner = null;
+    current = null;
+    player1 = new Player("X");
+    player2 = new Player("O");
+  });
 
-
+  // HELPERS
   function checkWinner(playerArr){
     var winFlag = false;
     if(playerArr.length < 3) {return false;}
@@ -83,7 +104,6 @@ $(document).ready(function() {
       }
       return true;
     };
-
     for(var j=0; j<winning.length; j++) {
       if (winFlag === false){
         winFlag = checkWinning(winning[j]);
